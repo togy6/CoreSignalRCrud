e5a3d7df4d2162bd41c0215307bf2694637670a7
togy <togy@DESKTOP-JC5EK17>
2021-10-23 13:26:11 +0300

    Add project files.

---
+++ CoreSignalRCrud/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CoreSignalRCrud.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreSignalRCrud.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+    }
+}
